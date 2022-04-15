69f12347a6fafa89eeefe0843ccc90f86f2a49f5
Mechwv <mechwv@gmail.com>
2022-04-15 07:22:23 +0300

    neco arc chaos documentation

---
+++ app/lib/game_manager.dart
@@ -0,0 +1,20 @@
+class GameManager {
+  void startGame() {
+    // TODO: implement startGame
+  }
+  void shufflePlayers() {
+    // TODO: implement shufflePlayers
+  }
+  void selectCard(int num) {
+    // TODO: implement selectCard
+  }
+  void endTurn() {
+    // TODO: implement endTurn
+  }
+  void writeHint(String text, int num) {
+    // TODO: implement writeHint
+  }
+  void _checkGameState() {
+    // TODO: implement _checkGameState
+  }
+}
---
+++ app/lib/settings.dart
@@ -0,0 +1,8 @@
+class Settings {
+  int teams;
+  int roundTime;
+  int cardCount;
+  int blackCardCount;
+  int whiteCardCount;
+  Settings(this.teams, this.roundTime, this.cardCount, this.blackCardCount, this.whiteCardCount);
+}
