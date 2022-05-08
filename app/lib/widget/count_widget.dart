c62680cdfa748f3a2ed8623efb975054d8fa85f5
BoringOwl <isergey555345@gmail.com>
2022-05-09 00:56:12 +0300

    counter init state

--- app/lib/widget/count_widget.dart
+++ app/lib/widget/count_widget.dart
@@ -5,15 +5,22 @@ import 'square_button.dart';
 import 'text_widget.dart';
 
 class CountWidget extends StatefulWidget {
-  const CountWidget({Key? key, required this.color, required this.text,}) : super(key: key);
+  const CountWidget({Key? key, required this.color, required this.text, required this.count,}) : super(key: key);
   final Color color;
   final String text;
-
+  final int count;
   @override
   State<CountWidget> createState() => _CountWidget();
 }
 class _CountWidget extends State<CountWidget> {
   int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    count = widget.count;
+  }
+
   @override
   Widget build(BuildContext context) {
     return FittedBox(
